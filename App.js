e6ca1f69e8e9bc0c03bcd273d0150d8fc98f5376
johnny <jbhall4291@gmail.com>
2023-03-25 22:48:03 +0000

    integrate MapView from react-native-maps

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { AppNavigator } from './components/AppNavigator';
-
-const CatalogScreen = () => (
-  <View style={styles.layout}>
-    <Text style={styles.title}>Catalog</Text>
-  </View>
-);
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
 
 
 const App = () => (
   <NavigationContainer>
-    <AppNavigator />
+    <AppNavigator />    
   </NavigationContainer>
 );
 
--- components/Gigs.jsx
+++ components/Gigs.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from "react-native-maps";
 
 export const Gigs = () => (
   <View style={styles.layout}>
     <Text style={styles.title}>Map Here!</Text>
+
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      initialRegion={{
+        latitude: 51.83113,
+        longitude: 1.11618,
+
+        // the two delta values below relate to the default 'zoom' level of map
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
   </View>
 );
 
@@ -19,5 +30,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
     marginBottom: 16,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
